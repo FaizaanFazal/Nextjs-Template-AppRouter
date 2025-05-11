3f95389d34f203f46f9df61e923c8918a377af8d
FaizaanFazal <dkfaizaan12@gmail.com>
2025-05-11 14:07:07 +0900

    fixed: localhost CSP voilation

--- src/components/section/SignInWithGithub/SignInWithGithub.tsx
+++ src/components/section/SignInWithGithub/SignInWithGithub.tsx
@@ -2,12 +2,24 @@
 import { signIn } from 'next-auth/react';
 
 export default function SignInWithGithub() {
-
+    const handleLogin = async () => {
+      const res = await signIn('github', {
+        redirect:    false,
+        callbackUrl: window.location.origin + '/'
+      });
+      if (res?.url) {
+        window.open(
+          res.url,
+          '_blank',
+          'width=600,height=700'
+        );
+      }
+    };
   return (
     <div style={{ padding: 16, textAlign: 'center' }}>
     <h1>Please sign in</h1>
     <button
-      onClick={() => signIn('github')}
+      onClick={handleLogin}
       style={{ marginTop: 12, padding: '8px 16px', fontSize: 16 }}
     >
       Sign in with GitHub
